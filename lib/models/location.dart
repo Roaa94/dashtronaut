21b6598e5acfe4eee48f6ed247f16dcbf294957d
roaa94 <roaa94khaddam@gmail.com>
2022-01-10 01:15:02 +0300

    added equatable package

--- lib/models/location.dart
+++ lib/models/location.dart
@@ -1,9 +1,22 @@
-class Location {
-  int x;
-  int y;
+import 'package:equatable/equatable.dart';
 
-  Location({
+class Location extends Equatable {
+  final int x;
+  final int y;
+
+  const Location({
     required this.x,
     required this.y,
   });
+
+  static void printLocations(List<Location> locations) {
+    Map<int, String> locationsMap = {};
+    for (int i = 0; i <= locations.length - 1; i++) {
+      locationsMap[i] = '(${locations[i].x}, ${locations[i].y})';
+    }
+    print(locationsMap);
+  }
+
+  @override
+  List<Object> get props => [x, y];
 }
