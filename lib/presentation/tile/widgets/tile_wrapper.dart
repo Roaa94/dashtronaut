4e70e1e287f3b5ec400f5fcacfe8f1cb5ade00aa
roaa94 <roaa94khaddam@gmail.com>
2022-01-10 20:01:27 +0300

    achieved animating tile to whitespace location on drag

--- lib/presentation/tile/widgets/tile_wrapper.dart
+++ lib/presentation/tile/widgets/tile_wrapper.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_puzzle_hack/models/position.dart';
+import 'package:flutter_puzzle_hack/models/puzzle.dart';
 import 'package:flutter_puzzle_hack/models/tile.dart';
 import 'package:flutter_puzzle_hack/presentation/providers/puzzle_provider.dart';
 import 'package:flutter_puzzle_hack/presentation/tile/widgets/tile_container.dart';
@@ -17,48 +19,66 @@ class TileWrapper extends StatefulWidget {
 }
 
 class _TileWrapperState extends State<TileWrapper> {
-  late double leftPosition;
-  late double topPosition;
+  late ValueNotifier<Position> tilePositionNotifier;
 
   @override
   void initState() {
-    leftPosition = widget.tile.position.left;
-    topPosition = widget.tile.position.top;
+    tilePositionNotifier = ValueNotifier<Position>(Position(
+      left: widget.tile.position.left,
+      top: widget.tile.position.top,
+    ));
     super.initState();
+  }
+
+  @override
+  void dispose() {
+    tilePositionNotifier.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Consumer<PuzzleProvider>(
       builder: (c, puzzleProvider, _) {
-        bool isMovableOnX = puzzleProvider.puzzle.tileIsMovableOnXAxis(widget.tile);
-        bool isMovableOnY = puzzleProvider.puzzle.tileIsMovableOnYAxis(widget.tile);
+        Puzzle puzzle = puzzleProvider.puzzle;
+        bool isMovableOnX = puzzle.tileIsMovableOnXAxis(widget.tile);
+        bool isMovableOnY = puzzle.tileIsMovableOnYAxis(widget.tile);
+        bool tileIsRightOfWhiteSpace = puzzle.tileIsRightOfWhiteSpace(widget.tile);
+        bool tileIsTopOfWhiteSpace = puzzle.tileIsTopOfWhiteSpace(widget.tile);
 
-        return AnimatedPositioned(
-          duration: const Duration(milliseconds: 1),
-          width: widget.tile.width,
-          height: widget.tile.width,
-          left: leftPosition,
-          top: topPosition,
-          child: GestureDetector(
-            onHorizontalDragUpdate: (DragUpdateDetails details) {
-              if (isMovableOnX) {
-                setState(() {
-                  leftPosition += details.delta.dx;
-                });
-              }
-            },
-            onVerticalDragUpdate: (DragUpdateDetails details) {
-              if (isMovableOnY) {
-                setState(() {
-                  topPosition += details.delta.dy;
-                });
-              }
-            },
-            child: TileContainer(
-              tile: widget.tile,
-              isTileMovable: isMovableOnX || isMovableOnY,
-              extraText: puzzleProvider.puzzle.getTileLocationText(widget.tile),
+        return ValueListenableBuilder(
+          valueListenable: tilePositionNotifier,
+          child: TileContainer(
+            tile: widget.tile,
+            isTileMovable: isMovableOnX || isMovableOnY,
+            extraText: puzzle.getTileLocationText(widget.tile),
+          ),
+          builder: (c, Position tilePosition, child) => AnimatedPositioned(
+            duration: const Duration(milliseconds: 100),
+            curve: Curves.easeInOut,
+            width: widget.tile.width,
+            height: widget.tile.width,
+            left: tilePosition.left,
+            top: tilePosition.top,
+            child: IgnorePointer(
+              ignoring: widget.tile.isWhiteSpaceTile,
+              child: GestureDetector(
+                onHorizontalDragUpdate: (DragUpdateDetails details) {
+                  if (isMovableOnX) {
+                    if ((tileIsRightOfWhiteSpace && details.delta.dx < 0) || (!tileIsRightOfWhiteSpace && details.delta.dx > 0)) {
+                      tilePositionNotifier.value = puzzle.whiteSpaceTile.position;
+                    }
+                  }
+                },
+                onVerticalDragUpdate: (DragUpdateDetails details) {
+                  if (isMovableOnY) {
+                    if ((tileIsTopOfWhiteSpace && details.delta.dy > 0) || (!tileIsTopOfWhiteSpace && details.delta.dy < 0)) {
+                      tilePositionNotifier.value = puzzle.whiteSpaceTile.position;
+                    }
+                  }
+                },
+                child: child!,
+              ),
             ),
           ),
         );
