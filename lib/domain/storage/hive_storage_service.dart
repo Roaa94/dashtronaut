1d0d02b7b9ae81d10411b3939994e883b9c0a30c
roaa94 <roaa94khaddam@gmail.com>
2022-02-23 11:22:36 +0300

    minor refactoring

--- lib/domain/storage/hive_storage_service.dart
+++ lib/domain/storage/hive_storage_service.dart
@@ -2,50 +2,46 @@ import 'package:flutter_puzzle_hack/domain/storage/storage_service.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
 class HiveStorageService implements StorageService {
-  late Box _hiveAppBox;
-
-  Future<void> _openBoxes() async {
-    _hiveAppBox = await Hive.openBox('dashtronaut');
-  }
+  late Box _hiveBox;
 
   @override
   Future<void> init() async {
     await Hive.initFlutter();
-    await _openBoxes();
+    _hiveBox = await Hive.openBox('dashtronaut');
   }
 
   @override
   Future<void> remove(String key) async {
-    _hiveAppBox.delete(key);
+    _hiveBox.delete(key);
   }
 
   @override
   dynamic get(String key) {
-    return _hiveAppBox.get(key);
+    return _hiveBox.get(key);
   }
 
   @override
   dynamic getAll() {
-    return _hiveAppBox.values.toList();
+    return _hiveBox.values.toList();
   }
 
   @override
   bool has(String key) {
-    return _hiveAppBox.containsKey(key);
+    return _hiveBox.containsKey(key);
   }
 
   @override
   Future<void> set(String? key, dynamic data) async {
-    _hiveAppBox.put(key, data);
+    _hiveBox.put(key, data);
   }
 
   @override
   Future<void> clear() async {
-    await _hiveAppBox.clear();
+    await _hiveBox.clear();
   }
 
   @override
   Future<void> clearBox() async {
-    await _hiveAppBox.clear();
+    await _hiveBox.clear();
   }
 }
