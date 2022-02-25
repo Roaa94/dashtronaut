ee82faec90be9a032a8c265d58ff5c84989681ff
roaa94 <roaa94khaddam@gmail.com>
2022-02-25 12:36:52 +0300

    wrote FileHelper tests

--- lib/helpers/file_helper.dart
+++ lib/helpers/file_helper.dart
@@ -2,10 +2,20 @@ import 'dart:io';
 
 import 'package:flutter/services.dart';
 import 'package:http/http.dart' as http;
-import 'package:path_provider/path_provider.dart';
+import 'package:path_provider/path_provider.dart' as path;
 
 /// Helper class for handling [File]s
 class FileHelper {
+  /// Get temporary directory
+  static Future<Directory> getTemporaryDirectory() async {
+    return await path.getTemporaryDirectory();
+  }
+
+  /// Write File as Bytes from path
+  static Future<File> writeFileAsBytes(dynamic byteData, String filePath) async {
+    return await File(filePath).writeAsBytes(byteData.buffer.asUint8List(byteData.offsetInBytes, byteData.lengthInBytes));
+  }
+
   /// Returns a type [File] from an asset path
   ///
   /// Writes the file's binary data to temporary location
@@ -14,7 +24,7 @@ class FileHelper {
     final byteData = await rootBundle.load(path);
     Directory _tempDirectory = await getTemporaryDirectory();
     String _tempPath = '${_tempDirectory.path}/$path';
-    return await File(_tempPath).writeAsBytes(byteData.buffer.asUint8List(byteData.offsetInBytes, byteData.lengthInBytes));
+    return await writeFileAsBytes(byteData, _tempPath);
   }
 
   /// Returns a type [File] from a url
@@ -27,6 +37,6 @@ class FileHelper {
     final byteData = response.bodyBytes;
     Directory _tempDirectory = await getTemporaryDirectory();
     String _tempPath = '${_tempDirectory.path}/file.png';
-    return await File(_tempPath).writeAsBytes(byteData.buffer.asUint8List(byteData.offsetInBytes, byteData.lengthInBytes));
+    return await writeFileAsBytes(byteData, _tempPath);
   }
 }
---
+++ test/mocks.dart
@@ -0,0 +1,6 @@
+import 'package:mocktail/mocktail.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:path_provider_platform_interface/path_provider_platform_interface.dart';
+import 'package:plugin_platform_interface/plugin_platform_interface.dart';
+
+class MockPathProviderPlatform extends Mock with MockPlatformInterfaceMixin implements PathProviderPlatform {}
