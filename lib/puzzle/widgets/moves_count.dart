0a8746995f9dea299b6fc1284c815e2225f99385
Roaa94 <roaa94khaddam@gmail.com>
2023-02-04 15:14:38 +0300

    chore: migrate MovesCount widget to use riverpod provider and test it

--- lib/puzzle/widgets/moves_count.dart
+++ lib/puzzle/widgets/moves_count.dart
@@ -1,26 +1,25 @@
 import 'package:dashtronaut/core/styles/app_text_styles.dart';
-import 'package:dashtronaut/puzzle/providers/old_puzzle_provider.dart';
+import 'package:dashtronaut/puzzle/providers/puzzle_moves_count_provider.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class MovesCount extends StatelessWidget {
+class MovesCount extends ConsumerWidget {
   const MovesCount({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return Selector<OldPuzzleProvider, int>(
-      selector: (c, puzzleProvider) => puzzleProvider.movesCount,
-      builder: (c, int movesCount, _) => RichText(
-        text: TextSpan(
-          text: 'Moves: ',
-          style: AppTextStyles.body.copyWith(color: Colors.white),
-          children: <TextSpan>[
-            TextSpan(
-              text: '$movesCount',
-              style: AppTextStyles.bodyBold.copyWith(color: Colors.white),
-            ),
-          ],
-        ),
+  Widget build(BuildContext context, WidgetRef ref) {
+    final movesCount = ref.watch(puzzleMovesCountProvider);
+
+    return RichText(
+      text: TextSpan(
+        text: 'Moves: ',
+        style: AppTextStyles.body.copyWith(color: Colors.white),
+        children: <TextSpan>[
+          TextSpan(
+            text: '$movesCount',
+            style: AppTextStyles.bodyBold.copyWith(color: Colors.white),
+          ),
+        ],
       ),
     );
   }
