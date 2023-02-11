b4aa3e98aac16d75ff9c5eb6d8f81a3b7d2d8b79
Roaa94 <roaa94khaddam@gmail.com>
2023-02-11 17:30:30 +0300

    chore: use riverpod provider in TileAnimatedPositioned widget

--- lib/puzzle/widgets/tile/tile_animated_positioned.dart
+++ lib/puzzle/widgets/tile/tile_animated_positioned.dart
@@ -1,24 +1,23 @@
 import 'package:dashtronaut/puzzle/models/position.dart';
 import 'package:dashtronaut/puzzle/models/tile.dart';
 import 'package:dashtronaut/core/layout/puzzle_layout.dart';
+import 'package:dashtronaut/puzzle/providers/puzzle_size_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class TileAnimatedPositioned extends StatelessWidget {
+class TileAnimatedPositioned extends ConsumerWidget {
   final Tile tile;
-  final bool isPuzzleSolved;
-  final int puzzleSize;
   final Widget tileGestureDetector;
 
   const TileAnimatedPositioned({
     super.key,
     required this.tile,
-    required this.isPuzzleSolved,
-    required this.puzzleSize,
     required this.tileGestureDetector,
   });
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final puzzleSize = ref.watch(puzzleSizeProvider);
     double tileWidth = PuzzleLayout(context).containerWidth / puzzleSize;
     Position tilePosition = tile.getPosition(context, tileWidth);
 
