8c318a5ad66d0812c4664f32c74d49b7094afa38
Roaa94 <roaa94khaddam@gmail.com>
2023-02-11 17:29:33 +0300

    feat: add & test PuzzleKeyboardListener widget

---
+++ lib/puzzle/widgets/puzzle_keyboard_listener.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:dashtronaut/puzzle/providers/tiles_provider.dart';
+
+class PuzzleKeyboardListener extends ConsumerStatefulWidget {
+  const PuzzleKeyboardListener({
+    super.key,
+    required this.child,
+  });
+
+  final Widget child;
+
+  @override
+  ConsumerState<PuzzleKeyboardListener> createState() =>
+      _PuzzleKeyboardListenerState();
+}
+
+class _PuzzleKeyboardListenerState
+    extends ConsumerState<PuzzleKeyboardListener> {
+  final FocusNode keyboardListenerFocusNode = FocusNode();
+
+  @override
+  Widget build(BuildContext context) {
+    return RawKeyboardListener(
+      onKey: (event) {
+        ref.read(tilesProvider.notifier).handleKeyboardEvent(event);
+      },
+      focusNode: keyboardListenerFocusNode,
+      child: Builder(
+        builder: (context) {
+          if (!keyboardListenerFocusNode.hasFocus) {
+            FocusScope.of(context).requestFocus(keyboardListenerFocusNode);
+          }
+          return widget.child;
+        },
+      ),
+    );
+  }
+}
---
+++ test/puzzle/widgets/puzzle_keyboard_listener_test.dart
@@ -0,0 +1,258 @@
+import 'package:dashtronaut/puzzle/models/location.dart';
+import 'package:dashtronaut/puzzle/models/puzzle.dart';
+import 'package:dashtronaut/puzzle/models/tile.dart';
+import 'package:dashtronaut/puzzle/repositories/puzzle_repository.dart';
+import 'package:dashtronaut/puzzle/widgets/puzzle_keyboard_listener.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+
+import '../../mocks.dart';
+import '../../utils/pump_app.dart';
+
+// 1  2
+//    3
+const tiles2x2AboveAndRightOfWhitespace = [
+  Tile(
+    value: 1,
+    currentLocation: Location(x: 1, y: 1),
+    correctLocation: Location(x: 1, y: 1),
+  ),
+  Tile(
+    value: 2,
+    currentLocation: Location(x: 2, y: 1),
+    correctLocation: Location(x: 2, y: 1),
+  ),
+  Tile(
+    value: 3,
+    currentLocation: Location(x: 2, y: 2),
+    correctLocation: Location(x: 1, y: 2),
+  ),
+  Tile(
+    value: 4,
+    tileIsWhiteSpace: true,
+    currentLocation: Location(x: 1, y: 2),
+    correctLocation: Location(x: 2, y: 2),
+  ),
+];
+
+// 1  2
+// 3
+const tiles2x2AboveAndLeftOfWhitespace = [
+  Tile(
+    value: 1,
+    currentLocation: Location(x: 1, y: 1),
+    correctLocation: Location(x: 1, y: 1),
+  ),
+  Tile(
+    value: 2,
+    currentLocation: Location(x: 2, y: 1),
+    correctLocation: Location(x: 2, y: 1),
+  ),
+  Tile(
+    value: 3,
+    correctLocation: Location(x: 1, y: 2),
+    currentLocation: Location(x: 1, y: 2),
+  ),
+  Tile(
+    value: 4,
+    tileIsWhiteSpace: true,
+    currentLocation: Location(x: 2, y: 2),
+    correctLocation: Location(x: 2, y: 2),
+  ),
+];
+
+// 1
+// 3  2
+const tiles2x2BelowAndLeftOfWhitespace = [
+  Tile(
+    value: 1,
+    currentLocation: Location(x: 1, y: 1),
+    correctLocation: Location(x: 1, y: 1),
+  ),
+  Tile(
+    value: 2,
+    currentLocation: Location(x: 2, y: 2),
+    correctLocation: Location(x: 2, y: 1),
+  ),
+  Tile(
+    value: 3,
+    correctLocation: Location(x: 1, y: 2),
+    currentLocation: Location(x: 1, y: 2),
+  ),
+  Tile(
+    value: 4,
+    tileIsWhiteSpace: true,
+    currentLocation: Location(x: 2, y: 1),
+    correctLocation: Location(x: 2, y: 2),
+  ),
+];
+
+//    1
+// 3  2
+const tiles2x2BelowAndRightOfWhitespace1 = [
+  Tile(
+    value: 1,
+    correctLocation: Location(x: 1, y: 1),
+    currentLocation: Location(x: 2, y: 1),
+  ),
+  Tile(
+    value: 2,
+    currentLocation: Location(x: 2, y: 2),
+    correctLocation: Location(x: 2, y: 1),
+  ),
+  Tile(
+    value: 3,
+    correctLocation: Location(x: 1, y: 2),
+    currentLocation: Location(x: 1, y: 2),
+  ),
+  Tile(
+    value: 4,
+    currentLocation: Location(x: 1, y: 1),
+    correctLocation: Location(x: 2, y: 2),
+    tileIsWhiteSpace: true,
+  ),
+];
+
+//    2
+// 1  3
+const tiles2x2BelowAndRightOfWhitespace2 = [
+  Tile(
+    value: 1,
+    correctLocation: Location(x: 1, y: 1),
+    currentLocation: Location(x: 1, y: 2),
+  ),
+  Tile(
+    value: 2,
+    currentLocation: Location(x: 2, y: 1),
+    correctLocation: Location(x: 2, y: 1),
+  ),
+  Tile(
+    value: 3,
+    currentLocation: Location(x: 2, y: 2),
+    correctLocation: Location(x: 1, y: 2),
+  ),
+  Tile(
+    value: 4,
+    currentLocation: Location(x: 1, y: 1),
+    tileIsWhiteSpace: true,
+    correctLocation: Location(x: 2, y: 2),
+  ),
+];
+
+void main() {
+  late PuzzleStorageRepository mockPuzzleRepository;
+
+  setUp(() {
+    mockPuzzleRepository = MockPuzzleStorageRepository();
+  });
+
+  testWidgets(
+    'arrowDown Key event moves tile top of whitespace',
+    (WidgetTester tester) async {
+      when(() => mockPuzzleRepository.get()).thenReturn(
+        const Puzzle(n: 2, tiles: tiles2x2AboveAndRightOfWhitespace),
+      );
+
+      await tester.pumpProviderApp(
+        PuzzleKeyboardListener(child: Container()),
+        overrides: [
+          puzzleRepositoryProvider.overrideWithValue(mockPuzzleRepository),
+        ],
+      );
+
+      await tester.sendKeyDownEvent(
+        LogicalKeyboardKey.arrowDown,
+        physicalKey: PhysicalKeyboardKey.arrowDown,
+      );
+
+      verify(
+        () => mockPuzzleRepository.updateTiles(
+          tiles2x2BelowAndRightOfWhitespace2,
+        ),
+      ).called(1);
+    },
+  );
+
+  testWidgets(
+    'arrowLeft Key event moves tile right of whitespace',
+    (WidgetTester tester) async {
+      when(() => mockPuzzleRepository.get()).thenReturn(
+        const Puzzle(n: 2, tiles: tiles2x2AboveAndRightOfWhitespace),
+      );
+
+      await tester.pumpProviderApp(
+        PuzzleKeyboardListener(child: Container()),
+        overrides: [
+          puzzleRepositoryProvider.overrideWithValue(mockPuzzleRepository),
+        ],
+      );
+
+      await tester.sendKeyDownEvent(
+        LogicalKeyboardKey.arrowLeft,
+        physicalKey: PhysicalKeyboardKey.arrowLeft,
+      );
+
+      verify(
+        () => mockPuzzleRepository.updateTiles(
+          tiles2x2AboveAndLeftOfWhitespace,
+        ),
+      ).called(1);
+    },
+  );
+
+  testWidgets(
+    'arrowUp Key event moves tile bottom of whitespace',
+    (WidgetTester tester) async {
+      when(() => mockPuzzleRepository.get()).thenReturn(
+        const Puzzle(n: 2, tiles: tiles2x2BelowAndLeftOfWhitespace),
+      );
+
+      await tester.pumpProviderApp(
+        PuzzleKeyboardListener(child: Container()),
+        overrides: [
+          puzzleRepositoryProvider.overrideWithValue(mockPuzzleRepository),
+        ],
+      );
+
+      await tester.sendKeyDownEvent(
+        LogicalKeyboardKey.arrowUp,
+        physicalKey: PhysicalKeyboardKey.arrowUp,
+      );
+
+      verify(
+        () => mockPuzzleRepository.updateTiles(
+          tiles2x2AboveAndLeftOfWhitespace,
+        ),
+      ).called(1);
+    },
+  );
+
+  testWidgets(
+    'arrowRight Key event moves tile left of whitespace',
+    (WidgetTester tester) async {
+      when(() => mockPuzzleRepository.get()).thenReturn(
+        const Puzzle(n: 2, tiles: tiles2x2BelowAndLeftOfWhitespace),
+      );
+
+      await tester.pumpProviderApp(
+        PuzzleKeyboardListener(child: Container()),
+        overrides: [
+          puzzleRepositoryProvider.overrideWithValue(mockPuzzleRepository),
+        ],
+      );
+
+      await tester.sendKeyDownEvent(
+        LogicalKeyboardKey.arrowRight,
+        physicalKey: PhysicalKeyboardKey.arrowRight,
+      );
+
+      verify(
+        () => mockPuzzleRepository.updateTiles(
+          tiles2x2BelowAndRightOfWhitespace1,
+        ),
+      ).called(1);
+    },
+  );
+}
