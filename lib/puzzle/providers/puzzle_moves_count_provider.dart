17cdcc710ecaeff1a81184b62c95d544f3549258
Roaa94 <roaa94khaddam@gmail.com>
2023-04-16 19:57:52 +0300

    refactor: movesCountProvider uses ref.select, and update tests

--- lib/puzzle/providers/puzzle_moves_count_provider.dart
+++ lib/puzzle/providers/puzzle_moves_count_provider.dart
@@ -2,7 +2,5 @@ import 'package:dashtronaut/puzzle/providers/puzzle_provider.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final puzzleMovesCountProvider = Provider<int>((ref) {
-  final tilesState = ref.watch(puzzleProvider);
-
-  return tilesState.movesCount;
+  return ref.watch(puzzleProvider.select((value) => value.movesCount));
 });
--- test/puzzle/providers/puzzle_moves_count_provider_test.dart
+++ test/puzzle/providers/puzzle_moves_count_provider_test.dart
@@ -1,4 +1,5 @@
 import 'package:dashtronaut/puzzle/providers/puzzle_moves_count_provider.dart';
+import 'package:dashtronaut/puzzle/providers/puzzle_provider.dart';
 import 'package:dashtronaut/puzzle/repositories/puzzle_repository.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
@@ -30,5 +31,63 @@ void main() {
       providerContainer.read(puzzleMovesCountProvider),
       equals(2),
     );
+  });
+
+  test('Value updated when puzzleProvider.movesCount value is updated',
+      () async {
+    when(() => mockPuzzleRepository.get()).thenReturn(puzzle2x2Solvable);
+
+    final movesCountListener = Listener<int>();
+
+    final providerContainer = ProviderContainer(
+      overrides: [
+        puzzleRepositoryProvider.overrideWithValue(mockPuzzleRepository),
+      ],
+    );
+    addTearDown(providerContainer.dispose);
+
+    providerContainer.listen(
+      puzzleMovesCountProvider,
+      movesCountListener,
+      fireImmediately: true,
+    );
+
+    providerContainer.read(puzzleProvider.notifier).swapTiles(
+          puzzle2x2Solvable.tiles.firstWhere((tile) => tile.value == 1),
+        );
+    await Future.delayed(Duration.zero);
+
+    verifyInOrder([
+      () => movesCountListener.call(null, 0),
+      () => movesCountListener.call(0, 1),
+    ]);
+  });
+
+  test('Value not updated when puzzleProvider.movesCount value is not updated',
+      () async {
+    when(() => mockPuzzleRepository.get()).thenReturn(puzzle2x2Solvable);
+
+    final movesCountListener = Listener<int>();
+
+    final providerContainer = ProviderContainer(
+      overrides: [
+        puzzleRepositoryProvider.overrideWithValue(mockPuzzleRepository),
+      ],
+    );
+    addTearDown(providerContainer.dispose);
+
+    providerContainer.listen(
+      puzzleMovesCountProvider,
+      movesCountListener,
+      fireImmediately: true,
+    );
+    verify(() => movesCountListener.call(null, 0));
+
+    // If moves count is already 0, resetting the puzzle should not update
+    // the movesCountProvider
+    providerContainer.read(puzzleProvider.notifier).reset();
+    await Future.delayed(Duration.zero);
+
+    verifyNoMoreInteractions(movesCountListener);
   });
 }
